54d51e5bf7b80b619b6722db319487774b31d093
thinh <21135681.thinh@student.iuh.edu.vn>
2025-11-28 14:49:39 +0700

    fix: change endpoint public

--- src/main/java/iuh/fit/se/configuration/SecurityConfig.java
+++ src/main/java/iuh/fit/se/configuration/SecurityConfig.java
@@ -22,7 +22,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final String[] PUBLIC_ENDPOINTS = {
-        "/review/**"
+            "/review/*"
     };
 
     private CustomJwtDecoder customJwtDecoder;
